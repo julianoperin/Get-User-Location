e42b85206a6cef55978662684e27ee80a778fe2a
‘Juliano <julianoperins@gmail.com>
2020-04-29 19:55:54 -0400

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.state = { lat: null }; //we dont know the lat yet that why null
+  }
+
+  //We have to define render
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position.coords.latitude),
+      err => console.log(err)
+    );
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
