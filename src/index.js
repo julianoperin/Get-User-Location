9d2db09d1181f645277ddabfab84992c39fa9817
‘Juliano <julianoperins@gmail.com>
2020-04-29 21:43:15 -0400

    Second Commit

--- src/index.js
+++ src/index.js
@@ -6,16 +6,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null }; //we dont know the lat yet that why null
+    this.state = { lat: null, errorMessage: "" }; //we dont know the lat yet that why null
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
 
   //We have to define render
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position.coords.latitude),
-      err => console.log(err)
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
     );
-    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
